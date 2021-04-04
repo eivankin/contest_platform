989896470e362b68bb11adb11a7c4c6c714f8463
Eugen <eug-potato@yandex.ru>
2021-04-04 23:07:15 +0300

    models WIP

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Contest(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    starts_at = models.DateTimeField()
+    ends_at = models.DateTimeField()
+    # TODO: renaming files & manage access (only for admins)
+    public_reference_file = models.FileField(blank=True, null=True)
+    private_reference_file = models.FileField(blank=True, null=True)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    leader_email = models.EmailField()
+    contest = models.ForeignKey('Contest', on_delete=models.CASCADE)
+    password = models.CharField(max_length=100)
+
+
+class Attempt(models.Model):
+    team = models.ForeignKey('Team', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # TODO: renaming file
+    file = models.FileField()
+    public_score = models.DecimalField(decimal_places=5, blank=True, null=True)
+    private_score = models.DecimalField(decimal_places=5, blank=True, null=True)
