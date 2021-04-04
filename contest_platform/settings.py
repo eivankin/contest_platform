6272daff38a6e0f9bf726bfb15ae1490bf231f95
Eugen <eug-potato@yandex.ru>
2021-04-04 18:48:56 +0300

    necessary additions for deployment

--- contest_platform/settings.py
+++ contest_platform/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -110,3 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
