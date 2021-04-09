c2e0c714e2e69e5a801329fb7b8fa12c8b3d3c81
Eugen <eug-potato@yandex.ru>
2021-04-09 23:08:19 +0300

    renaming attempt files on upload

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .utilities import get_pub_path, get_priv_path
+from .utilities import get_pub_path, get_priv_path, get_attempt_path
 
 
 class Contest(models.Model):
@@ -8,7 +8,6 @@ class Contest(models.Model):
     description = models.TextField()
     starts_at = models.DateTimeField()
     ends_at = models.DateTimeField()
-    # TODO: renaming files & manage access (only for admins)
     public_reference_file = models.FileField(blank=True, null=True, upload_to=get_pub_path)
     private_reference_file = models.FileField(blank=True, null=True, upload_to=get_priv_path)
     column_to_compare = models.CharField(max_length=50, null=True)
@@ -17,13 +16,12 @@ class Contest(models.Model):
 class Team(models.Model):
     name = models.CharField(max_length=100)
     contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User)
 
 
 class Attempt(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    # TODO: renaming file
-    file = models.FileField()
+    file = models.FileField(upload_to=get_attempt_path)
     public_score = models.FloatField(blank=True, null=True)
     private_score = models.FloatField(blank=True, null=True)
--- core/utilities.py
+++ core/utilities.py
@@ -11,3 +11,7 @@ def get_priv_path(instance, filename: str) -> str:
 
 def get_path(contest_id: int, file_ext: str, file_type: str) -> str:
     return f'contests/{contest_id}/{file_type}{file_ext}'
+
+
+def get_attempt_path(instance, filename: str):
+    return f'attempts/{instance.team.pk}/{instance.pk}{os.path.splitext(filename)[1]}'
