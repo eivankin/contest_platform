612aad0a595ada4733b71f57e96b81acb6482c6f
Eugen <eug-potato@yandex.ru>
2021-04-10 19:30:34 +0300

    check if contest is open before creating attempt, readme WIP

--- core/views.py
+++ core/views.py
@@ -11,24 +11,24 @@ from .serializers import TeamSerializer, ContestSerializer, \
 
 
 def active_contests() -> QuerySet:
-    """Returns QuerySet of active contests"""
+    """Returns QuerySet of active contests."""
     now = timezone.now()
     return Contest.objects.filter(ends_at__gt=now, starts_at__lte=now).order_by('-ends_at')
 
 
 def archive_contests() -> QuerySet:
-    """Returns QuerySet of archive contests"""
+    """Returns QuerySet of archive contests."""
     return Contest.objects.filter(ends_at__lte=timezone.now()).order_by('-ends_at')
 
 
 def future_contests() -> QuerySet:
-    """Returns QuerySet of future contest"""
+    """Returns QuerySet of future contest."""
     return Contest.objects.filter(starts_at__gt=timezone.now()).order_by('-starts_at')
 
 
 @api_view(['GET'])
 def contests(request: HttpRequest, contests_type: str = 'all') -> Response:
-    """Main contest view, returns json dictionary with requested contests or raises Http404"""
+    """Main contest view, returns json dictionary with requested contests or 404."""
     contests_types = {'all': lambda: Contest.objects.all(),
                       'active': active_contests,
                       'archive': archive_contests,
@@ -39,13 +39,17 @@ def contests(request: HttpRequest, contests_type: str = 'all') -> Response:
     contests_query = contests_types[contests_type]()
     if contests_type == 'my' and request.user.is_authenticated:
         contests_query = contests_query.filter(team__in=request.user.team_set.all())
-    serializer = ContestSerializer(contests_query, many=True)
-    return Response(serializer.data)
+    serialized = ContestSerializer(contests_query, many=True)
+    return Response(serialized.data)
 
 
 @login_required
 @api_view(['GET', 'POST'])
 def attempts(request: HttpRequest, contest_id: int = None) -> Response:
+    """
+    Returns list of attempts if request method is GET,
+    or creates new attempt if request method is post.
+    """
     if request.method == 'GET':
         serializer = ActiveContestAttemptSerializer
         attempts_query = Attempt.objects.filter(team__in=request.user.team_set.all())
@@ -68,6 +72,10 @@ def attempts(request: HttpRequest, contest_id: int = None) -> Response:
         except Exception:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
+        if contest.starts_at > timezone.now() or contest.ends_at < timezone.now():
+            return Response({'message': 'you can\'t create attempt in inactive contest'},
+                            status=status.HTTP_403_FORBIDDEN)
+
         data = {'team_id': request.user.team_set.get(contest=contest).pk,
                 'file': request.FILES.get('file')}
         serialized = NewAttemptSerializer(data=data)
@@ -79,5 +87,24 @@ def attempts(request: HttpRequest, contest_id: int = None) -> Response:
 
 
 @api_view(['GET'])
-def teams(request: HttpRequest) -> Response:
+def teams(request: HttpRequest, contest_id: int = None) -> Response:
+    """
+    Returns list of user teams if contest_id is None,
+    else returns all teams registered on contest
+    """
+    if contest_id is None:
+        teams_query = request.user.team_set.all()
+    else:
+        teams_query = Team.objects.filter(contest_id=contest_id)
+    serialized = TeamSerializer(teams_query, many=True)
+    return Response(serialized.data)
+
+
+@api_view(['GET'])
+def leaderboard(request: HttpRequest, contest_id: int) -> Response:
+    """
+    Returns list of teams with their best score.
+    If contest is finished, returns public and private leaderboard,
+    otherwise only public leaderboard will be returned.
+    """
     pass
