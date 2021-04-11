528a20b6964189af609f98ce8f54c958fa3bf562
Eugen <eug-potato@yandex.ru>
2021-04-11 16:36:56 +0300

    register form

--- front/forms.py
+++ front/forms.py
@@ -5,3 +5,8 @@ class AttemptForm(forms.Form):
     file = forms.FileField(label='Файл решения',
                            help_text='Не забудьте проверить систему координат и наличие нужного '
                                      'столбца в таблице атрибутов!')
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(label='Имя пользователя')
+    email = forms.EmailField()
